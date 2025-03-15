59df1ba27ee3367f860d73401888e350730aefa3
Bek95 <bbkey95@outlook.com>
2025-03-15 17:51:29 +0100

    paginate posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,9 +12,20 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        //on peut rajouter aussi des filtres et des options de tri si nécessaire
+        $posts = Post::paginate(10); // 10 posts par page
 
-        return response()->json($posts);
+        return response()->json([
+            'data' => $posts->items(),
+            'pagination' => [
+                'total' => $posts->total(),
+                'per_page' => $posts->perPage(),
+                'current_page' => $posts->currentPage(),
+                'last_page' => $posts->lastPage(),
+                'next_page_url' => $posts->nextPageUrl(),
+                'prev_page_url' => $posts->previousPageUrl(),
+            ]
+        ]);
     }
 
     public function show(Post $post)
@@ -41,9 +52,14 @@ class PostController extends Controller
     public function update(PostUpdateRequest $request, Post $post)
     {
         try {
-            $post->update($request->validated());
+            if ($post) {
+                $post->update($request->validated());
 
-            return response()->json(['message' => 'Post updated successfully'], 200);
+                return response()->json(['message' => 'Post updated successfully'], 200);
+            } else {
+                return response()->json(['message' => 'Post not found'], 404);
+            }
+
         } catch (\Exception $e) {
             return response()->json(['message' => 'Post cannot be updated ' . $e->getMessage()], 400);
         }
